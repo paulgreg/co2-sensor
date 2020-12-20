485939778d71967a80ff058952f5d3eac64e8e71
Grégory Paul <paulgreg@gmail.com>
2020-12-20 12:53:48 +0100

    Update more often

--- co2-sensor.ino
+++ co2-sensor.ino
@@ -20,7 +20,7 @@ void loop() {
   float humidity = getHumidity();
   unsigned int co2 = getCo2(temperature, humidity);
   displayData(temperature, humidity, co2);
-  if (serialIdx++ % 6 == 0) {
+  if (serialIdx++ % 12 == 0) {
     Serial.printf("Update: temp: %.1f c - humitidy: %.0f - co2: %i\n", temperature, humidity, co2);
     #ifdef WIFI_SSID
     if (connectToWifi(WIFI_SSID, WIFI_PASSWORD)) {
@@ -28,5 +28,5 @@ void loop() {
     }
     #endif
   }
-  delay(10 * 1000);
+  delay(5 * 1000);
 }
